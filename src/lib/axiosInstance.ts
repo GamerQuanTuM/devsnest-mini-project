31935d9ea1a13277f24d6c59e620432ad88f4d2f
GamerQuanTuM <shuvamsantra10@gmail.com>
2024-05-08 12:07:38 +0530

    prod url change

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const DEV_URL = "http://localhost:3000"
 
-const PROD_URL = "https://devsnest-mini-project.vercel.app"
+const PROD_URL = "https://devsnest-mini-project.onrender.com"
 
 export const axiosInstance = axios.create({
     baseURL: `${PROD_URL}/api`
