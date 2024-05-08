07251b6f3f7800b5123d5ab9a9584e1cb6524365
GamerQuanTuM <shuvamsantra10@gmail.com>
2024-05-08 10:55:39 +0530

    prod url domain added

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const DEV_URL = "http://localhost:3000"
 
-const PROD_URL = "https://devsnest-mini-project-git-master-gamerquantums-projects.vercel.app"
+const PROD_URL = "https://devsnest-mini-project.vercel.app"
 
 export const axiosInstance = axios.create({
     baseURL: `${PROD_URL}/api`
